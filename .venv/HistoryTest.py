785a8874c42415a4a752700d8e84bb70cc82161d
freezeal <freezealcho@gmail.com>
2024-06-14 08:11:07 +0900

    계산하기 스크립트 수정 및 히스토리 시나리오 추가

--- .venv/HistoryTest.py
+++ .venv/HistoryTest.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between, SequentialTaskSet
 import json
 import random
 
-class CalculatorScenario(HttpUser):
+class HistoryScenario(HttpUser):
     wait_time = between(1, 2)
 
     def on_start(self):
@@ -14,13 +14,8 @@ class CalculatorScenario(HttpUser):
     def on_stop(self):
         self.client.post("/logout_api")
 
-    #홈 화면 이동
-    @task
-    def Home(self):
-        self.client.get("/home")
-
-    #사칙 연산 시도
-    @task
+    #사칙연산 계산하기를 진행하는데 가중치를 설정
+    @task(10)
     def Calculator(self):
         num1 = random.randint(1, 2000)
         num2 = random.randint(1, 2000)
@@ -31,11 +26,16 @@ class CalculatorScenario(HttpUser):
         self.client.get(f"/calculator_api/{operator}", params=params)
 
     #히스토리 화면 이동
-    @task
+    @task(2)
     def HistoryView(self):
         self.client.get("/history_view")
 
     #히스토리 API 호출
-    @task
+    @task(2)
     def HistoryAPI(self):
         self.client.get("/history_api")
+
+    #히스토리 삭제 api 호출
+    @task(2)
+    def HistoryDel(self):
+        self.client.delete("/delete_history")
